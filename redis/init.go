499ba7f70e1cd8a41fa234b0805b6d925371d46f
Heylosky <2547226479@qq.com>
2023-01-10 17:41:15 +0800

    reuse redis client

--- redis/init.go
+++ redis/init.go
@@ -4,21 +4,32 @@ import (
 	"context"
 	"fmt"
 	redis "github.com/go-redis/redis/v8"
+	"go.uber.org/zap"
+	"time"
 )
 
-func GetToken(key string) (string, bool) {
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "172.25.240.10:30379",
-		Password: "Welcome@123",
-		DB:       0,
-	})
+var rClient *redis.Client
 
-	val, err := rdb.Get(context.Background(), key).Result()
+func init() {
+	rClient = redis.NewClient(&redis.Options{
+		Addr:               "172.25.240.10:30379",
+		Password:           "Welcome@123",
+		DB:                 0,
+		PoolSize:           30,
+		MinIdleConns:       10,
+		PoolTimeout:        3 * time.Second,
+		IdleCheckFrequency: 30 * time.Second,
+		IdleTimeout:        60 * time.Second,
+	})
+}
+
+func GetToken(key string) (string, bool) {
+	val, err := rClient.Get(context.Background(), key).Result()
 	if err != nil {
-		fmt.Println(err)
+		zap.L().Error("redis get error.", zap.Error(err))
 		return "", false
 	}
-	fmt.Println(val)
+	zap.L().Debug(val)
 
 	return val, true
 }
